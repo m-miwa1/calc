7a590202628461f35b426d53bfc383524b9003e3
MasakiMiwa <m-miwa@b-tm.co.jp>
2020-09-09 11:05:01 +0900

    calc2.php内の前画面から値が飛んできていない場合の分岐を修正

--- calc2.php
+++ calc2.php
@@ -1,58 +1,60 @@
 <?php
-    //計算機 表示された問題の解答を入力し、送信する画面
-    //前画面からPOSTで値が飛んできていない場合は何も表示しない
-    if (!empty($_POST["intDigit"])) :
-        //問題桁数
-        $intDigit = $_POST["intDigit"];
-        //演算指定
-        $intCalcMethod = 0;
-        //問題として出題される値の最大値
-        $maxDigit = 0;
+//計算機 表示された問題の解答を入力し、送信する画面
+//前画面からPOSTで値が飛んできていない場合は何も表示しない
+if (empty($_POST["intDigit"])){
+    exit;
+}
+//問題桁数
+$intDigit = $_POST["intDigit"];
+//演算指定
+$intCalcMethod = 0;
+//問題として出題される値の最大値
+$maxDigit = 0;
 
-        //出題する問題の値を格納する配列
-        $arrayQuestionA = [];
-        $arrayQuestionB = [];
+//出題する問題の値を格納する配列
+$arrayQuestionA = [];
+$arrayQuestionB = [];
         
-        //問題の答えを確認する配列
-        $arrayAnswer = [];
-        //問題を格納する配列
-        $arrayQuestion = [];
+//問題の答えを確認する配列
+$arrayAnswer = [];
+//問題を格納する配列
+$arrayQuestion = [];
 
-        require_once('const.php');
-        
-        //演算指定が未入力の場合、足し算とみなして1を格納
-        if (empty($_POST['intCalcMethod'])) {
-            $intCalcMethod = 1;
-        } else {
-        	//演算指定が片方・もしくは両方入力されていた場合、intCalcMethodに値を格納(足し算:1,引き算:2,両方:3)
-            foreach ($_POST['intCalcMethod'] as $intCalc) {
-                $intCalcMethod += $intCalc;
-            }
-        }
+require_once('const.php');
 
-        //forで各配列に値を格納
-        for ($x = 0; $x < NUMBER_QUESTION; $x++) {
-            $arrayQuestionA[] = mt_rand(MIN_DIGIT,MAX_DIGIT[$intDigit]);
-            $arrayQuestionB[] = mt_rand(MIN_DIGIT,MAX_DIGIT[$intDigit]);
-        }
+//演算指定が未入力の場合、足し算とみなして1を格納
+if (empty($_POST['intCalcMethod'])) {
+    $intCalcMethod = 1;
+} else {
+    //演算指定が片方・もしくは両方入力されていた場合、intCalcMethodに値を格納(足し算:1,引き算:2,両方:3)
+    foreach ($_POST['intCalcMethod'] as $intCalc) {
+        $intCalcMethod += $intCalc;
+    }
+}
+
+//forで各配列に値を格納
+for ($x = 0; $x < NUMBER_QUESTION; $x++) {
+    $arrayQuestionA[] = mt_rand(MIN_DIGIT,MAX_DIGIT[$intDigit]);
+    $arrayQuestionB[] = mt_rand(MIN_DIGIT,MAX_DIGIT[$intDigit]);
+}
         
-        //for内のifでintCalcMethodの値により足し算・引き算・両方(mt_rand()によってランダム)に分岐
-        //$arrayAnswerに問題の答えを、$arrayQuestionに問題を格納する
-        for ($i = 0; $i < NUMBER_QUESTION; $i++) {
-            if ($intCalcMethod == 1) {
-                $arrayAnswer[$i] = $arrayQuestionA[$i] + $arrayQuestionB[$i];
-                $arrayQuestion[$i] = $arrayQuestionA[$i] . " + " . $arrayQuestionB[$i] . " = ";
-            } elseif ($intCalcMethod == 2) {
-                $arrayAnswer[$i] = $arrayQuestionA[$i] - $arrayQuestionB[$i];
-                $arrayQuestion[$i] = $arrayQuestionA[$i] . " - " . $arrayQuestionB[$i] . " = ";
-            } elseif (mt_rand(1,2) % 2 == 0) {
-                $arrayAnswer[$i] = $arrayQuestionA[$i] - $arrayQuestionB[$i];
-                $arrayQuestion[$i] = $arrayQuestionA[$i] . " - " . $arrayQuestionB[$i] . " = ";
-            } else {
-                $arrayAnswer[$i] = $arrayQuestionA[$i] + $arrayQuestionB[$i];
-                $arrayQuestion[$i] = $arrayQuestionA[$i] . " + " . $arrayQuestionB[$i] . " = ";
-            }
-        }
+//for内のifでintCalcMethodの値により足し算・引き算・両方(mt_rand()によってランダム)に分岐
+//$arrayAnswerに問題の答えを、$arrayQuestionに問題を格納する
+for ($i = 0; $i < NUMBER_QUESTION; $i++) {
+    if ($intCalcMethod == 1) {
+        $arrayAnswer[$i] = $arrayQuestionA[$i] + $arrayQuestionB[$i];
+        $arrayQuestion[$i] = $arrayQuestionA[$i] . " + " . $arrayQuestionB[$i] . " = ";
+    } elseif ($intCalcMethod == 2) {
+        $arrayAnswer[$i] = $arrayQuestionA[$i] - $arrayQuestionB[$i];
+        $arrayQuestion[$i] = $arrayQuestionA[$i] . " - " . $arrayQuestionB[$i] . " = ";
+    } elseif (mt_rand(1,2) % 2 == 0) {
+        $arrayAnswer[$i] = $arrayQuestionA[$i] - $arrayQuestionB[$i];
+        $arrayQuestion[$i] = $arrayQuestionA[$i] . " - " . $arrayQuestionB[$i] . " = ";
+    } else {
+        $arrayAnswer[$i] = $arrayQuestionA[$i] + $arrayQuestionB[$i];
+        $arrayQuestion[$i] = $arrayQuestionA[$i] . " + " . $arrayQuestionB[$i] . " = ";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
