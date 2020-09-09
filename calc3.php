148f2fbd06452ac52a9239c925d3ae4c995bedda
MasakiMiwa <m-miwa@b-tm.co.jp>
2020-09-09 11:09:28 +0900

    calc3.php内の前画面から値が飛んできていない場合の分岐を修正

--- calc3.php
+++ calc3.php
@@ -1,26 +1,28 @@
 <?php
-    //計算機　問題の答えと入力された解答の答え合わせをする画面
-	//前画面からPOSTで値が飛んできていない場合は何も表示しない
-    if (!empty($_POST["arrayInputAnswer"])) :
-    	
-    	//入力された問題の解答を格納する配列
-        $arrayInputAnswer = $_POST['arrayInputAnswer'];
-        //問題を格納する配列
-        $arrayQuestion = unserialize($_POST['arrayQuestion']);
-        //問題の答えを格納する配列
-        $arrayAnswer = unserialize($_POST['arrayAnswer']);
-        //問題と入力された解答、正誤を格納する配列
-        $arrayQuestionAndAnswer = [];
+//計算機 問題の答えと入力された解答の答え合わせをする画面
+//前画面からPOSTで値が飛んできていない場合は何も表示しない
+if (empty($_POST["arrayInputAnswer"])) {
+    exit;
+}
 
-        for($n = 0; $n < 5; $n++){
-            $arrayQuestionAndAnswer[] = $n + 1 . "問目" . $arrayQuestion[$n] . $arrayInputAnswer[$n];
-            if ($arrayInputAnswer[$n] == $arrayAnswer[$n]) {
-                $arrayQuestionAndAnswer[] .= "...正解<br>";
-            } else {
-                $arrayQuestionAndAnswer[] .= "...不正解。答えは" . $arrayAnswer[$n] . "<br>";
-            }
+//入力された問題の解答を格納する配列
+$arrayInputAnswer = $_POST['arrayInputAnswer'];
+//問題を格納する配列
+$arrayQuestion = unserialize($_POST['arrayQuestion']);
+//問題の答えを格納する配列
+$arrayAnswer = unserialize($_POST['arrayAnswer']);
+//問題と入力された解答、正誤を格納する配列
+$arrayQuestionAndAnswer = [];
+
+for($n = 0; $n < 5; $n++){
+    $arrayQuestionAndAnswer[] = $n + 1 . "問目" . $arrayQuestion[$n] . $arrayInputAnswer[$n];
+    if ($arrayInputAnswer[$n] == $arrayAnswer[$n]) {
+        $arrayQuestionAndAnswer[] .= "...正解<br>";
+    } else {
+        $arrayQuestionAndAnswer[] .= "...不正解。答えは" . $arrayAnswer[$n] . "<br>";
+    }
             
-        }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -33,7 +35,5 @@
 <?php foreach ($arrayQuestionAndAnswer as $questionAndAnswer) : ?>
 <?php echo $questionAndAnswer ?>
 <?php endforeach; ?>
-
-<?php endif; ?>
 </body>
 </html>
