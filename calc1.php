128b27effbf2f8b034f6ace79ace2274c4c403c5
MasakiMiwa <m-miwa@b-tm.co.jp>
2020-09-11 12:54:29 +0900

    calc1.phpL22,23のinputのvalueの値を定数ADDITION,SUBSTRACTIONに変更

--- calc1.php
+++ calc1.php
@@ -19,8 +19,8 @@ require_once('const.php');
     </p>
     <p>
         演算指定（指定がないと足し算のみ行われます）<br>
-        <input type="checkbox" name="calcMethod[]" value="1">足し算<br>
-        <input type="checkbox" name="calcMethod[]" value="2">引き算
+        <input type="checkbox" name="calcMethod[]" value="<?php echo ADDITION ?>">足し算<br>
+        <input type="checkbox" name="calcMethod[]" value="<?php echo SUBSTRACTION ?>">引き算
     </p>
     <input type="submit" value="送信">
     <input type="reset" value="リセット"><br>
