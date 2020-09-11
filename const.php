46318051f3766552fd5b6d60a64d9f092d9879a5
MasakiMiwa <m-miwa@b-tm.co.jp>
2020-09-11 12:35:31 +0900

    const.phpの定数CALC_METHODを削除。演算方法の定数を3つ追加

--- const.php
+++ const.php
@@ -8,5 +8,7 @@ define('NUMBER_QUESTION', 5);
 //問題として出題される数字の最大値
 define('MAX_DIGIT', [1 => 9, 2 => 99]);
 
-//四則演算を格納する配列
-define('CALC_METHOD', [1 => "足し算", 2 => "引き算", 3 => "足し算引き算"]);
+//四則演算を格納する定数
+define('ADDITION', 1);
+define('SUBSTRACTION', 2);
+define('ADDITION_SUBSTRACTION', 3);
